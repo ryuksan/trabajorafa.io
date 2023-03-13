88fa20d67d629312e51f8b8f5ec69dfe456e6b2b
ryuksan <clinicasystemlab@gmail.com>
2023-03-13 04:24:39 -0500

    se sube pagina inicio con css y javascript

---
+++ asistencia/js/script.js
@@ -0,0 +1,70 @@
+var clic = 1;
+function adult(){ 
+   if(clic==1){
+   document.getElementById("adult").style.display = "block";
+   document.getElementById("chaturbate").style.display = "none";
+   document.getElementById("stripchat").style.display = "none";
+   document.getElementById("streamate").style.display = "none";
+   document.getElementById("sender").style.display = "none";
+   } else{
+    document.getElementById("adult").style.display = "none";      
+    clic = 1;
+   }   
+}
+function chaturbate(){ 
+  if(clic==1){
+  document.getElementById("chaturbate").style.display = "block";
+  document.getElementById("stripchat").style.display = "none";
+  document.getElementById("adult").style.display = "none";
+  document.getElementById("streamate").style.display = "none";
+  document.getElementById("sender").style.display = "none";
+  } else{
+   document.getElementById("chaturbate").style.display = "none";      
+   clic = 1;
+  }   
+}
+function stripchat(){ 
+  if(clic==1){
+  document.getElementById("stripchat").style.display = "block";
+  document.getElementById("chaturbate").style.display = "none";
+  document.getElementById("adult").style.display = "none";
+  document.getElementById("streamate").style.display = "none";
+  document.getElementById("sender").style.display = "none";
+  } else{
+   document.getElementById("stripchat").style.display = "none";      
+   clic = 1;
+  }   
+}
+function streamate(){ 
+  if(clic==1){
+  document.getElementById("streamate").style.display = "block";
+  document.getElementById("chaturbate").style.display = "none";
+  document.getElementById("stripchat").style.display = "none";
+  document.getElementById("adult").style.display = "none";
+  document.getElementById("sender").style.display = "none";
+  } else{
+   document.getElementById("streamate").style.display = "none";      
+   clic = 1;
+  }   
+}
+function sender(){ 
+  if(clic==1){
+  document.getElementById("sender").style.display = "block";
+  document.getElementById("chaturbate").style.display = "none";
+  document.getElementById("stripchat").style.display = "none";
+  document.getElementById("adult").style.display = "none";
+  document.getElementById("streamate").style.display = "none";
+  } else{
+   document.getElementById("sender").style.display = "none";      
+   clic = 1;
+  }   
+}
+/*function opacity0(){ 
+  if(clic==1){
+  document.getElementById("sender").style.opacity = "none";
+  document.getElementById("chaturbate").style.opacity = "none";
+  document.getElementById("stripchat").style.opacity = "none";
+  document.getElementById("adult").style.opacity = "none";
+  document.getElementById("streamate").style.opacity = "none";
+  }   
+}*/
